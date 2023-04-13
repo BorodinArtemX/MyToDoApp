3f27f04227c001409f977097b749c53c520639d0
BorodinArtemX <BorodinArtemX@icloud.com>
2023-04-13 17:20:59 +0200

    Create Project & Delete Storyboard

---
+++ MyToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDoApp
+//
+//  Created by Артем Бородин on 13/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
